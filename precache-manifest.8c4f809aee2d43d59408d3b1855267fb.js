5d55fc83bd76122c5a598a2a10ce11148b06f085
mathangspk <mathangspk@gmail.com>
2020-04-24 16:25:58 +0700

    first commit

---
+++ precache-manifest.8c4f809aee2d43d59408d3b1855267fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f880658f0be61c9bbe0151b0d367c98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae9cef811fc0e0a0a179",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "ae9cef811fc0e0a0a179",
+    "url": "/static/js/2.d7a51c4b.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/static/js/2.d7a51c4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8723f752e4b02eeebb66",
+    "url": "/static/js/main.7d9d4414.chunk.js"
+  },
+  {
+    "revision": "147c03f6fc5d5cb30b5b",
+    "url": "/static/js/runtime-main.eb8a4d72.js"
+  },
+  {
+    "revision": "c4c1f06492ead5c53bba21d5414672fa",
+    "url": "/static/media/loading.c4c1f064.gif"
+  }
+]);
